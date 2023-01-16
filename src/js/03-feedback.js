0d491f000907392af2ebacfc18798d7d535b8952
AlexTigra1974 <106554517+AlexTigra1974@users.noreply.github.com>
2023-01-17 00:57:27 +0200

    update task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,41 +1,11 @@
-// import throttle from 'lodash.throttle';
-
-// const STORAGE_KEY = 'feedback-form-state';
-// let formData = {};
-// const form = document.querySelector('.feedback-form');
-// console.dir(form);
-
-// form.addEventListener('submit', onFormSubmit);
-// form.addEventListener('input', onFormInput);
-
-// populateForm();
-
-// function onFormSubmit(event) {
-//   event.preventDefault();
-//   event.currentTarget.reset();
-
-//   localStorage.removeItem(STORAGE_KEY);
-// }
-
-// function onFormInput(event) {
-//   console.log(event.target);
-//   // formData = console.log(formData);
-//   // localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-// }
-
-// function populateForm() {
-//   if (localStorage.getItem(STORAGE_KEY));
-
-//   // formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-//   // console.log(form.elements);
-// }
 import throttle from 'lodash.throttle';
-
-const STORAGE_KEY = 'feedback-form-state';
-let formData = {};
 
 const form = document.querySelector('.feedback-form');
 console.dir(form);
+
+const STORAGE_KEY = 'feedback-form-state';
+let formData = {};
+console.log(formData);
 
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', throttle(onFormInput, 500));

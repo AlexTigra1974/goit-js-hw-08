108e4d5ef2cf9c78a7abc2d5d1eb48179f7229c6
AlexTigra1974 <106554517+AlexTigra1974@users.noreply.github.com>
2023-01-16 03:06:21 +0200

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,52 +1,20 @@
-import player from '@vimeo/player';
+import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-// {
-//   /* <iframe src="https://player.vimeo.com/video/76979871?h=8272103f6e" width="640" height="360" frameborder="0" allowfullscreen allow="autoplay; encrypted-media"></iframe>
-
-// <script src="https://player.vimeo.com/api/player.js"></script> */
-// }
-
-// const iframe = document.querySelector('iframe');
+const iframe = document.querySelector('iframe');
 // console.dir(iframe);
 
-// const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 // console.dir(player);
+const KEY_TIME = 'videoplayer-current-time';
 
-// iframe.addEventListener('click', onPlay);
-// const onPlay = function (data) {
-//   // data is an object containing properties specific to that event
-// };
-
-// player.on('play', onPlay);
-
-// player.on('play', function () {
-//   console.log('played the video!');
-// });
-// const onPlay = function (data) {
-//   // data is an object containing properties specific to that event
-// };
-
-// player.on('play', onPlay);
-
-// // If later on you decide that you don’t need to listen for play anymore.
-// player.off('play', onPlay);
-
-// // Alternatively, `off` can be called with just the event name to remove all
-// // listeners.
-// player.off('play');
-
-// player.getVideoTitle().then(function (title) {
-//   console.log('title:', title);
-// });
-// player
-//   .getCurrentTime()
-//   .then(function (seconds) {
-//     // seconds = the current playback position
-//   })
-//   .catch(function (error) {
-//     // an error occurred
-//   });
-// const KEY_TIME = currentTime.seconds;
-// localStorage.setItem(KEY_TIME);
-// const time = localStorage.getItem(KEY_TIME);
+player.on(
+  'timeupdate',
+  throttle(function ({ seconds }) {
+    localStorage.setItem(KEY_TIME, seconds);
+  }, 1000)
+);
+const time = localStorage.getItem(KEY_TIME);
+if (time) {
+  player.setCurrentTime(time);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,3 +1,34 @@
+// import throttle from 'lodash.throttle';
+
+// const STORAGE_KEY = 'feedback-form-state';
+// let formData = {};
+// const form = document.querySelector('.feedback-form');
+// console.dir(form);
+
+// form.addEventListener('submit', onFormSubmit);
+// form.addEventListener('input', onFormInput);
+
+// populateForm();
+
+// function onFormSubmit(event) {
+//   event.preventDefault();
+//   event.currentTarget.reset();
+
+//   localStorage.removeItem(STORAGE_KEY);
+// }
+
+// function onFormInput(event) {
+//   console.log(event.target);
+//   // formData = console.log(formData);
+//   // localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+// }
+
+// function populateForm() {
+//   if (localStorage.getItem(STORAGE_KEY));
+
+//   // formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+//   // console.log(form.elements);
+// }
 import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
@@ -19,6 +50,8 @@ function onFormSubmit(e) {
 }
 
 function onFormInput(e) {
+  // console.log(e.target.name);
+  // console.log(e.target.value);
   formData[e.target.name] = e.target.value;
   console.log(formData);
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
